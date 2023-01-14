72859714473012bf4d7938c5cf5d570ef1470e37
codingcrisis <codingcrisis@proton.me>
2023-01-14 22:43:15 +0100

    Initial commit of the tutorial

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Just to hide warnings, don't use in production
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Hello, world!");
+}
